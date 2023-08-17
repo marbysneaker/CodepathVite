44121a5e75317b9e4ba5e91760c79c0c90bfb33b
marbypangilinan <marbysneaker@gmail.com>
2023-08-16 23:28:48 -0700

    added component and pages

---
+++ vite-project/src/client.js
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = 'https://weyzzruooybhjhelvaon.supabase.co';
+
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndleXp6cnVvb3liaGpoZWx2YW9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIyNTA5OTMsImV4cCI6MjAwNzgyNjk5M30.AaHU1doc03MyHOR7dlrX3Lfvjce2m_-g-mZ7mZhMzsw';
+
+const supabase = createClient(URL, API_KEY);
+
+
+export const supabase = createClient(URL, API_KEY);
+
