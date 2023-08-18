9acc4c4a120aa6c0ca72efbc0039eae2ea413f15
Marcelino Pangilinan <marbysneaker@gmail.com>
2023-08-17 17:48:25 -0700

    added textfield

--- vite-project/src/components/CreatorCard.jsx
+++ vite-project/src/components/CreatorCard.jsx
@@ -4,7 +4,7 @@ const CreatorCard = (props) => {
     const {creator} = props
     const {index} = props
   return (
-    <div key={index}>
+    <div >
       <h1>{creator.name}</h1>
       <img src={creator.imageURL} alt="" />
     </div>
--- vite-project/src/pages/Addcreator.jsx
+++ vite-project/src/pages/Addcreator.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Texfield from '@mui/material/TextField';
+import './Addcreator.css'
 
 const Addcreator = () => {
   return (
+    <div className="Addcreator">
+    
     <div>
       Add creator
-      I am here
+     
     </div>
+    <div className="input-container">
+      <Texfield id="standard-basic" label="Name" />
+      <Texfield id="standard-basic" label="URL" />
+      <Texfield id="standard-basic" label="Description" />
+      <Texfield id="standard-basic" label="ImageURL" />
+    </div>
+    
+    </div>
+
   )
 }
 
